5651e50f861fdaad09b7ecf27122bad1127fcd72
Jasmin Mehic <mehic.jasmin@gmail.com>
2013-02-28 20:14:59 -0500

    fixing up some formatting

--- lime/app.js
+++ lime/app.js
@@ -42,34 +42,35 @@ var app = express();
 //};
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  //app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'ejs');
-  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    //app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'ejs');
+    app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/users', user.list);
 app.get('/database', function( req, res ){
-  res.render('database.ejs', { title: 'Lime - Database Demo'});
+    res.render('database.ejs', { title: 'Lime - Database Demo'});
 });
 app.post('/database', function( req, res ){
-  console.log("posted");
-  console.log(JSON.stringify(req.body));
-  db.users.insert(req.body);
-  res.render('database.ejs', { title: 'Lime - Database Demo'});
+    console.log("posted");
+    console.log(JSON.stringify(req.body));
+    db.users.insert(req.body);
+    //res.render('database.ejs', { title: 'Lime - Database Demo'});
+    res.redirect('/database');
 });
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
