cd612df3877401be0a83b52d392aea1dfcea03ff
Jasmin Mehic <mehic.jasmin@gmail.com>
2013-02-28 19:05:26 -0500

    simple database demo

--- lime/app.js
+++ lime/app.js
@@ -7,9 +7,39 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , db_url = "coconut"
+  , collections = ["users"]
+  , db = require('mongojs').connect(db_url, collections);
+
+  //, db = require('mongodb').db
+  //, db_connection = require('mongodb').connection
+  //, db_server = require('mongodb').server;
+
+//var host = process.env['MONGO_NODE_DRIVER_HOST'] !== null ?
+//            process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+//var port = process.env['MONGO_NODE_DRIVER_HOST'] !== null ?
+//            process.env['MONGO_NODE_DRIVER_HOST'] : db_connection.DEFAULT_PORT;
 
 var app = express();
+
+//function runQuery( db, query, nextFn ){
+  //  db.open( function( err, db ){
+    //    db.collection( myCollection, function( err, collection ){
+      //      collection.find( query, function( err, cursor ){
+        //        cursor.toArray( function( err, docs ){
+          //          console.log("Found " + docs.length + " documents");
+            //        var queryResults = [];
+              //      for( var i = 0; i < docs.length; i ++ ){
+                //        queryResults[queryResults.length] = docs[i];
+                  //  }
+                    //db.close();
+                    //nextFn(queryResults);
+                //});
+            //});
+        //});
+    //});
+//};
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -28,8 +58,17 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/database', function( req, res ){
+  res.render('database.ejs', { title: 'Lime - Database Demo'});
+});
+app.post('/database', function( req, res ){
+  console.log("posted");
+  console.log(JSON.stringify(req.body));
+  db.users.insert(req.body);
+  res.render('database.ejs', { title: 'Lime - Database Demo'});
+});
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- lime/routes/index.js
+++ lime/routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index.ejs', { title: 'Lime' });
+  res.render('index.ejs', { title: 'Lime - Home' });
 };
