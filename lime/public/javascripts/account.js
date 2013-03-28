ac0f240659e35c0de12ddaab7a374ee8687014e5
Jasmin Mehic <mehic.jasmin@gmail.com>
2013-03-28 12:31:33 -0400

    work in progress commit added jQuery functionality to account page

--- lime/public/javascripts/account.js
+++ lime/public/javascripts/account.js
@@ -1,3 +1,48 @@
 $(document).ready(function(){
-    $('.menu').accordion();
+    var $wel = $('#welmsg');
+    var $inventory = $('.inventory');
+    var $inventory_item = $('.inventory-item');
+    var $stats = $('.stats>*');
+    var $report = $('.report>*');
+    var $edit_inv = $('.edit-inventory');
+    $edit_inv.hide();
+    $report.hide();
+    $stats.hide();
+    $inventory.hide();
+    $inventory_item.hide();
+    $('#inv').on('click', function(){
+        $wel.fadeOut('slow');
+        $edit_inv.fadeOut('slow');
+        $stats.fadeOut('slow');
+        $report.fadeOut('slow');
+        $inventory.fadeIn('slow');
+        $inventory_item.fadeIn('slow');
+    });
+
+    $('#tds').on('click', function(){
+        $wel.fadeOut('slow');
+        $edit_inv.fadeOut('slow');
+        $inventory.fadeOut('slow');
+        $inventory_item.fadeOut('slow');
+        $report.fadeOut('slow');
+        $stats.fadeIn('slow');
+    });
+
+    $('#gen').on('click', function(){
+        $wel.fadeOut('slow');
+        $edit_inv.fadeOut('slow');
+        $inventory.fadeOut('slow');
+        $inventory_item.fadeOut('slow');
+        $stats.fadeOut('slow');
+        $report.fadeIn('slow');
+    });
+
+    $('#edit').on('click', function(){
+        $wel.fadeOut('slow');
+        $inventory.fadeOut('slow');
+        $inventory_item.fadeOut('slow');
+        $stats.fadeOut('slow');
+        $report.fadeOut('slow');
+        $edit_inv.fadeIn('slow');
+    });
 });
